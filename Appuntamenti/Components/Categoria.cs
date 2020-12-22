3ce7f0a83b97e671ecbd105bef41a9d10160fbcd
LeoamrosIV <mrlucke11@gmail.com>
2020-12-22 13:15:37 +0100

    2020/12/18 lesson update. Added 'Categorie'.

--- Appuntamenti/Components/Categoria.cs
+++ Appuntamenti/Components/Categoria.cs
@@ -2,12 +2,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace Appuntamenti.Components
 {
     public class Categoria
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "Inserire nome categoria")]
         public string Nome { get; set; }
     }
 }

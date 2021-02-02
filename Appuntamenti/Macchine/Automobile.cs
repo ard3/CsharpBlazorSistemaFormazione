2057c293116800e58b880354653d260fa2ede664
LeoamrosIV <mrlucke11@gmail.com>
2021-02-02 12:54:41 +0100

    Added ParcoMacchine

---
+++ Appuntamenti/Macchine/Automobile.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Appuntamenti.Macchine
+{
+    public class Automobile
+    {
+        public int Id;
+
+        [Required(ErrorMessage = "Inserire targa")]
+        public string Targa;
+
+        public string Modello;
+    }
+}
---
+++ Appuntamenti/Macchine/Prenotazione.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Appuntamenti.Macchine
+{
+    public class Prenotazione
+    {
+        public int Id;
+
+        [Required(ErrorMessage = "Inserire auto")]
+        public int IdAuto;
+
+        [Required(ErrorMessage = "Inserire nome")]
+        public string Chi;
+
+        [Required(ErrorMessage = "Inserire data di inizio prenotazione")]
+        public DateTime Inizio;
+
+        [Required(ErrorMessage = "Inserire data di fine prenotazione")]
+        public DateTime Fine;
+
+        [Required(ErrorMessage = "Inserire destinazione")]
+        public string Dove;
+    }
+}

bc06385422f58f0bd7e61b277ddb8db7881203a8
LeoamrosIV <mrlucke11@gmail.com>
2021-02-12 20:00:14 +0100

    Added FaceICT proto-socialnetwork

---
+++ Appuntamenti/FaceICT/Post.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Appuntamenti.FaceICT
+{
+    public class Post
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Inserire nome utente")]
+        public string Utente { get; set; }
+
+        public DateTime Data { get; set; } = DateTime.Now;
+
+        [Required(ErrorMessage = "Inserire il contenuto del post")]
+        public string Contenuto { get; set; }
+
+        public bool Modificato { get; set; } = false;
+    }
+}
